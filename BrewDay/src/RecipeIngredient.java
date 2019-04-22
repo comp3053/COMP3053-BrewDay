458e7a171b6d6c76b1c85df780b0ae3c692bc5c0
GriffinChen <1123306966@qq.com>
2019-04-22 16:33:31 +0800

    add: basic code of 2 class, RecipeIngredient and StorageIngredien

--- BrewDay/src/RecipeIngredient.java
+++ BrewDay/src/RecipeIngredient.java
@@ -1,4 +1,19 @@
 
 public class RecipeIngredient {
+	private int idOfRecipeIngredient;
+	private String nameOfRecipeIngredient;
+	private float amountOfRecipeIngredient;
+	
+	public boolean updateAmount() {
+		
+	}
+	
+	public boolean updateName() {
+		
+	}
+	
+	public boolean delete() {
+		
+	}
 
 }
--- BrewDay/src/StorageIngredient.java
+++ BrewDay/src/StorageIngredient.java
@@ -1,4 +1,16 @@
 
 public class StorageIngredient {
+	private int idOfStorageIngredient;
+	private String nameOfStorageIngredient;
+	private float amountOfStorageIngredient;
+	private char unitOfStorageIngredient;
+	
+	public boolean substractAmount() {
+		
+	}
+	
+	public boolean addAmount() {
+		
+	}
 
 }
