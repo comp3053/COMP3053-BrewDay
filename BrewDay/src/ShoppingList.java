d9d6576b92e2e66a23bb2257b556117828aa0086
Jimmy <jimmygplus@foxmail.com>
2019-04-22 15:10:08 +0800

    Jimmy's shoppinglist update

--- BrewDay/src/ShoppingList.java
+++ BrewDay/src/ShoppingList.java
@@ -1,4 +1,19 @@
+import java.util.Date;
 
-public class ShoppingList {
+public class ShoppingList{
 
+	private String nameOfIngredient = null;
+	
+	private int numberOfIngredient = 0;
+	
+	private Date shoppingDate = null;
+	
+	private int idOfIngredient;
+	
+	public String getNameOfIngredient(idofIngredient);
+	
+	public int getNumberOfIngredient(idOfIngredient);
+	
+	public Date getShoppingDATE(idOfIngredient);
+	
 }
