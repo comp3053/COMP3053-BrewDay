600540c16bfca52139b2b73aa5924393b5f4b792
l630003023 <l630003023@CZ148073.UTC.local>
2019-05-14 16:52:39 +0800

    The update ingredient page

--- brewDay/src/brewDay/View/IngredientPage.java
+++ brewDay/src/brewDay/View/IngredientPage.java
@@ -6,16 +6,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class IngredientPage extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
+
 
 	/**
 	 * Launch the application.
@@ -39,73 +46,74 @@ public class IngredientPage extends JFrame {
 	public IngredientPage() {
 		setTitle("Ingredient page");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(200, 200, 450, 300);
+		setBounds(200, 200, 525, 367);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton_1 = new JButton("Update");
-		btnNewButton_1.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnNewButton_1.setForeground(new Color(50, 205, 50));
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_1.setBounds(148, 115, 133, 45);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Delete");
-		btnNewButton_2.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnNewButton_2.setForeground(new Color(255, 0, 0));
-		btnNewButton_2.setBounds(148, 172, 133, 45);
-		contentPane.add(btnNewButton_2);
-		
-		JLabel lblNewLabel = new JLabel("<html>*Press the buttons to operate!*</html>");
+		JLabel lblNewLabel = new JLabel("<html>Input the recipe name that you want to check:</html>");
+		lblNewLabel.setBounds(134, 11, 271, 34);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel.setBounds(148, 39, 133, 16);
 		contentPane.add(lblNewLabel);
 		
-		JButton btnAdd = new JButton("Add");
-		btnAdd.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnAdd.setForeground(new Color(30, 144, 255));
-		btnAdd.setBounds(148, 58, 133, 45);
-		contentPane.add(btnAdd);
-		btnAdd.addActionListener(new ActionListener(){
+		JButton btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnBack.setForeground(new Color(30, 144, 255));
+		btnBack.setBounds(28, 14, 68, 29);
+		contentPane.add(btnBack);
+		
+		btnBack.addActionListener(new ActionListener(){
         	public void actionPerformed(ActionEvent e) {
         	setVisible(false);
 
-        	JFrame addRE = new AddRecipePage();
-        	addRE.setLocation(100,50);
-        	addRE.setSize(600, 500);
-        	addRE.setVisible(true);
+        	JFrame MaintainR = new MaintainRecipePage();
+        	MaintainR.setLocation(100,50);
+        	MaintainR.setSize(600, 500);
+        	MaintainR.setVisible(true);
         	}
 
         	});
-		btnNewButton_1.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent e) {
-        	setVisible(false);
-
-        	JFrame upRE = new UpdateRecipePage();
-        	upRE.setLocation(100,50);
-        	upRE.setSize(600, 500);
-        	upRE.setVisible(true);
-        	}
-
-        	});
-		btnNewButton_2.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent e) {
-        	setVisible(false);
-
-        	JFrame deRE = new DeleteRecipePage();
-        	deRE.setLocation(100,50);
-        	deRE.setSize(600, 500);
-        	deRE.setVisible(true);
-        	}
-
-        	});
-
+		
+		JLabel lblSelectRecipe = new JLabel("Select recipe:");
+		lblSelectRecipe.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectRecipe.setBounds(93, 70, 91, 16);
+		contentPane.add(lblSelectRecipe);
+		
+		JScrollPane scrollPane = new JScrollPane();            
+		scrollPane.setBounds(41, 105, 436, 185);
+		contentPane.add(scrollPane);
+		
+		Object[] columnNames =	{"Recipe name", "Amount", "Unit"};
+		Object[][] rowData = {
+				{"beer", 1, 'l'},
+				{"dasd", 6, 'g'},
+				{"asd", 6, 'g'},
+				{"asd", 6, 'g'},
+				{"asd", 6, 'g'},
+				{"fsdght", 6, 'g'},
+				{"yfgfst", 6, 'g'},
+				{"yeast", 6, 'g'},
+				{"yeast", 6, 'g'},
+				{"yeast", 6, 'g'},
+				{"yeast", 6, 'g'}
+        };
+		
+		table = new JTable(rowData, columnNames);
+		table.setBackground(new Color(255, 182, 193));
+		scrollPane.add(table.getTableHeader());
+		scrollPane.add(table);
+		
+		scrollPane.setViewportView(table);
+		
+		textField = new JTextField();
+		textField.setBounds(176, 65, 166, 29);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton.setBounds(346, 65, 49, 29);
+		contentPane.add(btnNewButton);
     }
-	
-    
 }
