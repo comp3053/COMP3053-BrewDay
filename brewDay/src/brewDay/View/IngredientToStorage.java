98a8f580a4190db19786f7c417e9046d90ee7cfd
Jimmy <jimmygplus@foxmail.com>
2019-05-20 01:37:10 +0800

    update the ingredeinttostorage

--- brewDay/src/brewDay/View/IngredientToStorage.java
+++ brewDay/src/brewDay/View/IngredientToStorage.java
@@ -24,6 +24,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Vector;
+import javax.swing.JTextField;
 
 public class IngredientToStorage extends JFrame {
 
@@ -31,6 +32,9 @@ public class IngredientToStorage extends JFrame {
 	private JTable table;
 	private JButton btnBack;
 	private JLabel lblNewLabel;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -56,7 +60,7 @@ public class IngredientToStorage extends JFrame {
 		
 		setTitle("Storage Page");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 428);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -66,7 +70,7 @@ public class IngredientToStorage extends JFrame {
 		scrollPane.setBounds(54, 53, 345, 183);
 		contentPane.add(scrollPane);
 		
-		Vector<String> columnName = new Vector<String>();//×Ö¶ÎÃû
+		Vector<String> columnName = new Vector<String>();//ï¿½Ö¶ï¿½ï¿½ï¿½
 		Vector<Vector<Object>> dataVector = new
 		Vector<Vector<Object>>();
 		columnName.add("name");
@@ -94,6 +98,43 @@ public class IngredientToStorage extends JFrame {
 		button.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		button.setBounds(12, 12, 68, 29);
 		contentPane.add(button);
+		
+		textField = new JTextField();
+		textField.setBounds(200, 274, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblIngredientName = new JLabel("<html>Ingredient name:</html>");
+		lblIngredientName.setBounds(92, 271, 121, 33);
+		contentPane.add(lblIngredientName);
+		
+		JLabel lblingredeintAmount = new JLabel("<html>Ingredeint amount:</html>");
+		lblingredeintAmount.setBounds(92, 297, 121, 33);
+		contentPane.add(lblingredeintAmount);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(210, 300, 120, 26);
+		contentPane.add(textField_1);
+		
+		JLabel lblingredientUnit = new JLabel("<html>Ingredient unit:</html>");
+		lblingredientUnit.setBounds(92, 326, 121, 33);
+		contentPane.add(lblingredientUnit);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(200, 329, 130, 26);
+		contentPane.add(textField_2);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setForeground(new Color(50, 205, 50));
+		btnSubmit.setBackground(new Color(255, 255, 255));
+		btnSubmit.setBounds(170, 371, 117, 29);
+		contentPane.add(btnSubmit);
+		
+		JLabel lblNewLabel_1 = new JLabel("<html>Fill in the blank for the ingredient that you want to add</html>");
+		lblNewLabel_1.setBounds(64, 243, 335, 29);
+		contentPane.add(lblNewLabel_1);
 		
 		button.addActionListener(new ActionListener(){
         	public void actionPerformed(ActionEvent e) {
